820bb92e14ccafb049b75d98f22721bbf2a63b5f
piyush.gyl <piyush2022ug@gmail.com>
2025-04-11 09:17:19 +0530

    chore: allowed origin

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,6 @@
-const allowedOrigins = ["http://localhost:5173"];
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://playground-040-frontend.vercel.app",
+];
 
 module.exports = { allowedOrigins };
